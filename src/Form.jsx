a0e971f98351007de31aaf85056678e438570590
Dev-kalathiya <devfullstack461@gmail.com>
2024-07-17 19:59:16 +0530

    form-validation

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,5 +1,6 @@
-
 import React, { useState } from "react";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Form = () => {
   const [userData, setUserData] = useState({
@@ -13,46 +14,89 @@ const Form = () => {
     let { name, value } = e.target;
     setUserData({ ...userData, [name]: value });
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    let usernameRegex = /^[a-zA-Z0-9._-]+$/;
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+    let { username, phone, email, password } = userData;
+
+    if (!passwordRegex.test(password)) {
+      toast.error("Password is invalid (at least 8 characters with uppercase, lowercase, and digit)", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+      });
+      return;
+
+    }
+
+   else{
+    toast.success("Login successful", {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    transition: Bounce,
+  });}
     
+
     console.log(userData);
   };
+
   return (
     <div className="main">
+      <ToastContainer />
       <form onSubmit={handleSubmit}>
+        <p className="hexagon"><span>Form</span></p>
         <input
           type="text"
           value={userData.username}
           name="username"
-          placeholder="enter-name"
+          placeholder="Enter username"
           onChange={handleInputData}
+          required
         />
         <input
           className="d1"
           type="text"
           value={userData.phone}
           name="phone"
-          placeholder="phone-number"
+          placeholder="Phone number"
           onChange={handleInputData}
+          required
         />
         <input
           className="d2"
           type="text"
           value={userData.email}
           name="email"
-          placeholder="email"
+          placeholder="Email"
           onChange={handleInputData}
+          required
         />
         <input
           className="d3"
-          type="text"
+          type="password"
           value={userData.password}
           name="password"
-          placeholder="password"
+          placeholder="Password"
           onChange={handleInputData}
+          required
         />
-        <input className="button" type="submit" onClick={handleInputData} />
+        <input className="button" type="submit" value="Submit" />
       </form>
     </div>
   );
